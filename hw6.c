400e95f6376584cf2432fb8a686fde814a152998
Ryeongeun-Kim <127407431+Ryeongeun-Kim@users.noreply.github.com>
2023-11-03 14:02:34 +0900

    Add files via upload

---
+++ hw6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+    int arr[5];
+    int i;
+    printf("Please input five integers:");
+    for(i=0; i<5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Odd numbers: ");
+	for(i=0; i<5; i++)
+	{
+		if(arr[i] % 2 == 1)
+        printf("%d ", arr[i]);
+	}
+    printf("\nEven numbers: ");
+	for(i=0; i<5; i++)
+	{
+		if(arr[i] % 2 == 0)
+        printf("%d ", arr[i]);
+	}
+    return 0;
+}
